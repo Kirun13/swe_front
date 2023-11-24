f98cd312e78f5305fdb49d4ddb949fe387515a35
Nurtore Onggarbay <nurtore.onggarbay@nu.edu.kz>
2023-11-24 12:33:43 +0600

    edited driver pages

--- app.py
+++ app.py
@@ -7,13 +7,27 @@ app = flask.Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/driver')
-def driver():
-    return render_template("driver/driver.html")
-
 @app.route('/login')
 def login():
     return render_template("login.html")
+
+
+@app.route('/driver')
+def driver():
+    return render_template("internal/driver/driver_main.html")
+
+@app.route('/driver_active_routes')
+def driver_active_routes():
+    return render_template("internal/driver/driver_active_routes.html")
+
+@app.route('/driver_routes_history')
+def driver_routes_history():
+    return render_template("internal/driver/driver_routes_history.html")
+
+@app.route('/driver_route_details')
+def driver_route_details():
+    return render_template("internal/driver/driver_route_details.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
